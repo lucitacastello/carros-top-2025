55543b133b80ac207e82e9ca4dc9121a8902a06e
Cida Rosa <lucita.rosa@gmail.com>
2025-11-16 18:19:40 -0300

    feat: SearchBar complete

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -4,26 +4,20 @@ import Header from "../../components/Header";
 import SearchBar from "../../components/SearchBar";
 
 export default function Catalog() {
+  return (
+    <>
+      <Header />
+      <main className="ct-catalog-main">
+        <section id="ct-catalog-section" className="ct-container">
+          <div className="ct-search-bar-container">
+            <SearchBar />
+          </div>
 
-    return (
-
-        <>
-            <Header />
-            <main className="ct-catalog-main">
-                <section id="ct-catalog-section">
-                    <div className="ct-search-bar-container">
-                        <SearchBar />
-                    </div>
-
-                    <div className="ct-catalogs-card-container">
-                        <div>Teste</div>
-
-                    </div>
-
-
-                </section>
-            </main>
-
-        </>
-    )
+          <div className="ct-catalog-cards-container">
+            <div>Teste</div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
